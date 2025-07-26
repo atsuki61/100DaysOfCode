16d31e24c8ec06486fb616cbfc4b7fca4dea103f
atsuki <0601m0429@gmail.com>
2025-07-26 14:21:30 +0900

    fix: Day25 headerとpageheaderのタイトルを修正

--- src/app/day25-current-location-map/layout.tsx
+++ src/app/day25-current-location-map/layout.tsx
@@ -15,8 +15,8 @@ export default function Day25Layout({
 }>) {
   return (
     <>
-      <Header title="100DaysOfCodeチャレンジ" /> {/* titleプロパティを追加 */}
-      <PageHeader title="Day 25 - 現在地マップ" description="Geolocation APIと地図ライブラリを使って現在地を地図上に表示するアプリ" /> {/* titleとdescriptionプロパティを追加 */}
+      <Header title="Day 25: 現在地マップ" /> {/* titleプロパティを追加 */}
+      <PageHeader title="現在地マップ" description="Geolocation APIと地図ライブラリを使って現在地を地図上に表示するアプリ" /> {/* titleとdescriptionプロパティを追加 */}
       <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-50"> {/* 背景色と最小高さを設定 */}
         {children}
       </main>
